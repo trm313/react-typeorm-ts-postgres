966b82067001750f3957bc1aefe96374c6a8b1c2
Taylor <trm313@gmail.com>
2020-02-08 19:00:28 -0500

    Updated Tailwindcss to 1.2.0 for transitions. Added Line Icons via CDN

--- client/src/components/TestComponent.js
+++ client/src/components/TestComponent.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+let styles = {
+  p: "flex items-center uppercase text-lg",
+  div: "bg-white shadow p-4 text-gray-600 rounded-lg"
+};
+
 const TestComponent = () => {
   const [data, setData] = useState(null);
 
@@ -17,11 +22,29 @@ const TestComponent = () => {
     fetchData();
   }, []);
 
+  if (data && data === "ERROR") styles.p += " text-red-500";
+  if (data && data !== "ERROR") styles.p += " text-green-600";
+
   return (
-    <div className="bg-white shadow p-4 text-gray-600 rounded-lg">
-      {!data && <p>Testing data fetch</p>}
-      {data === "ERROR" && <p className="text-red-500">Error fetching data</p>}
-      {data && data !== "ERROR" && <p>Data fetching succeeded</p>}
+    <div className={styles.div}>
+      {!data && (
+        <p className={styles.p}>
+          <i className="lni-spinner-arrow mr-2 spin-inf" />
+          Testing data fetch
+        </p>
+      )}
+      {data === "ERROR" && (
+        <p className={styles.p}>
+          <i className="lni-warning mr-2" />
+          Error fetching data
+        </p>
+      )}
+      {data && data !== "ERROR" && (
+        <p className={styles.p}>
+          <i className="lni-check-mark-circle mr-2" />
+          Data fetching succeeded
+        </p>
+      )}
     </div>
   );
 };
